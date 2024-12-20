c724afcdc7dfaf5234d0a98fa1e4258cda419df0
Jan Ziemba <ziemba.jan@icloud.com>
2024-12-20 13:02:08 +0100

    feat: buttons

---
+++ buttons/AdvancedButton.tsx
@@ -0,0 +1,182 @@
+import { ReactElement } from "react";
+import {
+	ActivityIndicator,
+	ColorValue,
+	Pressable,
+	StyleSheet,
+	Text,
+	TextStyle,
+	View,
+	ViewStyle,
+} from "react-native";
+import { theme } from "./theme";
+
+export type ButtonColor = "primary" | "danger" | "success" | "transparent";
+export type ButtonSize = "small" | "medium" | "large";
+
+export interface AdvancedButtonProps {
+	accessibilityHint?: string;
+	accessibilityLabel?: string;
+	color?: ButtonColor;
+	Icon?: ReactElement;
+	isDisabled?: boolean;
+	isLoading?: boolean;
+	onPress: () => void;
+	size?: ButtonSize;
+	title: string;
+}
+
+const styles = StyleSheet.create({
+	container: {
+		alignItems: "center",
+		borderRadius: 8,
+		flexDirection: "row",
+		gap: 8,
+		justifyContent: "center",
+	},
+	title: {
+		flexShrink: 1,
+		fontWeight: "600",
+	},
+});
+
+const colorVariants: Record<
+	ButtonColor,
+	{
+		backgroundColor: ColorValue;
+		activeBackgroundColor: ColorValue;
+		titleColor: ColorValue;
+		activeTitleColor: ColorValue;
+	}
+> = {
+	primary: {
+		backgroundColor: theme.colors.primary,
+		activeBackgroundColor: theme.colors.primaryActive,
+		titleColor: theme.colors.textInverted,
+		activeTitleColor: theme.colors.textInverted,
+	},
+	danger: {
+		backgroundColor: theme.colors.danger,
+		activeBackgroundColor: theme.colors.dangerActive,
+		titleColor: theme.colors.textInverted,
+		activeTitleColor: theme.colors.textInverted,
+	},
+	success: {
+		backgroundColor: theme.colors.success,
+		activeBackgroundColor: theme.colors.successActive,
+		titleColor: theme.colors.textInverted,
+		activeTitleColor: theme.colors.textInverted,
+	},
+	transparent: {
+		backgroundColor: "transparent",
+		activeBackgroundColor: "transparent",
+		titleColor: theme.colors.primary,
+		activeTitleColor: theme.colors.textPrimary,
+	},
+};
+
+const sizeVariants: Record<
+	ButtonSize,
+	{
+		container: ViewStyle;
+		title: TextStyle;
+	}
+> = {
+	small: {
+		container: {
+			height: 36,
+			paddingHorizontal: 12,
+			paddingVertical: 8,
+		},
+		title: {
+			fontSize: 14,
+		},
+	},
+	medium: {
+		container: {
+			height: 42,
+			paddingHorizontal: 12,
+			paddingVertical: 8,
+		},
+		title: {
+			fontSize: 18,
+		},
+	},
+	large: {
+		container: {
+			height: 52,
+			paddingHorizontal: 16,
+			paddingVertical: 12,
+		},
+		title: {
+			fontSize: 20,
+		},
+	},
+};
+
+export const AdvancedButton = ({
+	accessibilityHint,
+	accessibilityLabel,
+	color = "primary",
+	Icon,
+	isDisabled = false,
+	isLoading = false,
+	onPress,
+	size = "medium",
+	title,
+}: AdvancedButtonProps) => {
+	return (
+		<Pressable
+			accessibilityHint={accessibilityHint}
+			accessibilityLabel={accessibilityLabel}
+			accessibilityRole="button"
+			accessibilityState={{
+				busy: isLoading,
+				disabled: isDisabled || isLoading,
+			}}
+			disabled={isDisabled || isLoading}
+			hitSlop={16}
+			onPress={onPress}
+		>
+			{({ pressed }) => (
+				<View
+					style={[
+						styles.container,
+						sizeVariants[size].container,
+						{
+							backgroundColor: pressed
+								? colorVariants[color].activeBackgroundColor
+								: colorVariants[color].backgroundColor,
+							opacity: isDisabled ? 0.5 : 1,
+						},
+					]}
+				>
+					{isLoading ? (
+						<ActivityIndicator
+							color={colorVariants[color].titleColor}
+							size={sizeVariants[size].title.fontSize}
+						/>
+					) : (
+						<>
+							{Icon}
+							<Text
+								numberOfLines={1}
+								style={[
+									styles.title,
+									sizeVariants[size].title,
+									{
+										color: pressed
+											? colorVariants[color].activeTitleColor
+											: colorVariants[color].titleColor,
+									},
+								]}
+							>
+								{title}
+							</Text>
+						</>
+					)}
+				</View>
+			)}
+		</Pressable>
+	);
+};
---
+++ buttons/App.tsx
@@ -0,0 +1,89 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useState } from "react";
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import { AdvancedButton } from "./AdvancedButton";
+import { SimpleButton } from "./SimpleButton";
+import { theme } from "./theme";
+
+const styles = StyleSheet.create({
+	safeArea: {
+		backgroundColor: theme.colors.backgroundPrimary,
+	},
+	container: {
+		alignSelf: "center",
+		gap: 16,
+		maxWidth: 300,
+		padding: 16,
+		paddingBottom: 100,
+	},
+	heading: {
+		color: theme.colors.textPrimary,
+		fontSize: 24,
+		fontWeight: "bold",
+		marginTop: 16,
+		textAlign: "center",
+	},
+});
+
+const App = () => {
+	const [isLoading, setIsLoading] = useState(false);
+
+	const title = "Submit";
+
+	return (
+			<SafeAreaView style={styles.safeArea}>
+				<ScrollView contentContainerStyle={styles.container}>
+					<Text style={styles.heading}>Simple button</Text>
+					<SimpleButton
+						isLoading={isLoading}
+						onPress={() => {
+							// simulation of form submission
+							setIsLoading(true);
+							setTimeout(() => {
+								setIsLoading(false);
+							}, 2000);
+						}}
+						title={title}
+					/>
+					<SimpleButton
+						Icon={
+							<MaterialCommunityIcons
+								color={theme.colors.textInverted}
+								name="send"
+								size={18}
+							/>
+						}
+						onPress={() => {}}
+						title={title}
+					/>
+					<SimpleButton isLoading onPress={() => {}} title={title} />
+					<SimpleButton isDisabled onPress={() => {}} title={title} />
+					<View style={{ flexDirection: "row", justifyContent: "center" }}>
+						<SimpleButton onPress={() => {}} title={title} />
+					</View>
+
+					<Text style={styles.heading}>Advanced button</Text>
+					<AdvancedButton onPress={() => {}} title="Primary" />
+					<AdvancedButton
+						onPress={() => {}}
+						size="large"
+						title="Primary large"
+					/>
+					<AdvancedButton
+						onPress={() => {}}
+						size="small"
+						title="Primary small"
+					/>
+					<AdvancedButton color="success" onPress={() => {}} title="Success" />
+					<AdvancedButton color="danger" onPress={() => {}} title="Danger" />
+					<AdvancedButton
+						color="transparent"
+						onPress={() => {}}
+						title="Transparent"
+					/>
+				</ScrollView>
+			</SafeAreaView>
+	);
+};
+
+export default App;
